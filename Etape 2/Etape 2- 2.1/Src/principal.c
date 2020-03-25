fdc1c18673d9022184f426e1a9a5e578e326dc86
yacine180496 <benchehi@etud.insa-toulouse.fr>
2020-03-25 11:31:50 +0100

    modification étape 2.1

--- Etape 2/Etape 2- 2.1/Src/principal.c
+++ Etape 2/Etape 2- 2.1/Src/principal.c
@@ -1,13 +1,22 @@
 int tab[64];
-int s_carre(int);	
+int s_carre(int a);	
+int max = 0 ; 
+int min = 0x7ffffff ; 
 
 int main(void)
 {
+	int b = 0 ; 
 	for (int i=0;i<64;i++){
-		tab[i]=s_carre(i);
+		b = s_carre(i) ; 
+		
+		if(b>max){
+			max = b ; 
+		}
+		if(b<min){
+			min = b ; 
+		}
 	}
-	
 	while (1) {
 	
 	}
-}
+} 
